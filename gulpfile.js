03715bf28d724851c60c14ea388158f8aba2b920
Antonio Garcia Font <60427703+Toni908@users.noreply.github.com>
2021-12-10 13:09:31 +0100

    main

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,45 @@
 'use strict';
 
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass')(require('sass'));
-const { watch, series } = require('gulp');
+const imagemin = require("gulp-imagemin");
+const { series } = require('gulp');
 
 function defaultTask(cb) {
     // place code for your default task here
     cb();
   }
 
-function buildStyles() {
-  return gulp.src('./lib/bootstrap-5.1.3/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./lib/bootstrap-5.1.3/dist/css'));
-};
-
-function buildStyles2() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
-
-function vetllarSCSS(cb){
-    watch('./lib/bootstrap-5.1.3/scss/**/*.scss', buildStyles);
-    // watch('cssM/*.css', minifyStyles)
-    cb();
+  function minifyStyles() {
+    return gulp.src('./css/**/*.css')
+      .pipe(cleanCSS().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/css'));
   }
 
+  function minifyJs() {
+    return gulp.src('./js/**/*.js')
+      .pipe(uglify().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/js'));
+  }
+
+  function minifyhtml() {
+    return gulp.src('./*.html')
+      .pipe(htmlmin().on('error', sass.logError))
+      .pipe(gulp.dest('./dist'));
+  }
+
+function minifyimg(){
+  return gulp.src("./img/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img/.'));
+}
+  
   exports.default = defaultTask
-  exports.build = buildStyles2
-  exports.sasswatch = vetllarSCSS
+  exports.minimitzacss = minifyStyles
+  exports.minimitzajs = minifyJs
+  exports.minimitzahtml = minifyhtml
+  exports.minimitzaimg = minifyimg
+  exports.all = series(minifyStyles, minifyJs, minifyhtml, minifyimg)
